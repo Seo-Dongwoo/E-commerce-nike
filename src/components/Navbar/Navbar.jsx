3db76aba718747b5446758ccc71290b51fbd680e
seo-dongwoo <ehdrntdnzzz@naver.com>
2022-03-23 20:01:52 +0900

    ecommerce-nike

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  Typography,
+} from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
+import { Link, useLocation } from "react-router-dom";
+import logo from "../../assets/NIKE.jpg";
+import useStyles from "./styles";
+
+const Navbar = ({ totalItems }) => {
+  const classes = useStyles();
+  const location = useLocation();
+
+  return (
+    <>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <Typography
+            component={Link}
+            to="/"
+            variant="h6"
+            className={classes.title}
+            color="inherit"
+          >
+            <img
+              src={logo}
+              alt="Commerce.js"
+              height="40px"
+              className={classes.image}
+            />
+          </Typography>
+          <div className={classes.grow} />
+          {location.pathname === "/" && (
+            // '/' 문자 뒤 URL의 경로를 값으로 하는 DOMString입니다.
+            <div className={classes.button}>
+              <IconButton
+                component={Link}
+                to="/cart"
+                aria-label="Show cart items"
+                color="inherit"
+              >
+                <Badge badgeContent={totalItems} color="secondary">
+                  <ShoppingCart />
+                </Badge>
+              </IconButton>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Navbar;
